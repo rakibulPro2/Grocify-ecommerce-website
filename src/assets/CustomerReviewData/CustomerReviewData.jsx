2f2f8d0ae51bf430803b35a9b35aa17ddbb9d78b
rakibulPro2 <info.rakib33@gmail.com>
2026-06-06 05:08:00 -0700

    Customer Review

---
+++ src/assets/CustomerReviewData/CustomerReviewData.jsx
@@ -0,0 +1,53 @@
+import Customer1 from '../../assets/customer1.jpg'
+import Customer2 from '../../assets/customer2.jpg'
+import Customer3 from '../../assets/customer3.jpg'
+import Customer4 from '../../assets/customer4.jpg'
+import Customer5 from '../../assets/customer5.jpg'
+
+
+
+const CustomerReviewData = [
+    {
+        id: 1,
+        name: 'Emily Johnson',
+        profession: 'Food Blogger',
+        rating: 4,
+        para: 'FreshBasket is my go-to store for all grocery needs. Their produce is always fresh, and the delivery is super fast. I love the user-friendly interface and variety of organic options!',
+        image: Customer1
+    },
+    {
+        id: 2,
+        name: 'David Smith',
+        profession: 'Chef',
+        rating: 3,
+        para: 'As a chef, quality ingredients are everything. FreshBasket consistently delivers the best vegetables, herbs, and pantry staples. Highly recommended!',
+        image: Customer2
+    },
+    {
+        id: 3,
+        name: 'Alya Zahra',
+        profession: 'Model',
+        rating: 5,
+        para: "Shopping online with FreshBasket has saved me so much time. I trust them for my family's weekly groceries—always fresh, affordable, and reliable.",
+        image: Customer3
+    },
+    {
+        id: 4,
+        name: 'Carlos Mendes',
+        profession: 'Fitness Coach',
+        rating: 4,
+        para: 'I appreciate the selection of healthy foods and clean-label products. FreshBasket has been a great partner in my wellness journey!',
+        image: Customer4
+    },
+    {
+        id: 5,
+        name: 'Natcha Phongchai',
+        profession: 'Nutritionist',
+        rating: 3,
+        para: 'FreshBasket offers high-quality groceries at reasonable prices. Their organic section is impressive, and their customer service is top-notch.',
+        image: Customer5
+    },
+]
+
+
+export default CustomerReviewData
--- src/components/Testomonials/Testomonials.jsx
+++ src/components/Testomonials/Testomonials.jsx
@@ -1,24 +1,76 @@
-import React from 'react'
-import HeadinLine from '../HeadinLine/HeadinLine'
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import React from "react";
+import HeadinLine from "../HeadinLine/HeadinLine";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import CustomerReviewData from "../../assets/CustomerReviewData/CustomerReviewData";
+// swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { FaStar } from "react-icons/fa";
 
 const Testomonials = () => {
   return (
     <section>
-        <div className="max-w-[1300px] px-10 py-20">
+      <div className="max-w-[1300px] md:px-10 px-5 md:py-20 py-10">
         <HeadinLine highlight="Customer" heading="Review" />
-        <div className="flex justify-end gap-3">
-            <button className='w-9 h-9 text-2xl flex justify-center items-center text-zinc-800 bg-zinc-200 hover:bg-orange-500 hover:text-white rounded-sm '  >
-                <IoIosArrowBack />
-            </button>
-            <button className='w-9 h-9  text-2xl flex justify-center items-center text-zinc-800 bg-zinc-200 hover:bg-orange-500 hover:text-white rounded-sm '>
-                <IoIosArrowForward />
-            </button>
 
+        {/* Arrow icon */}
+        <div className="flex justify-end gap-3 md:mb-5 mb-3">
+          <button className="customPrev w-9 h-9 text-2xl flex justify-center items-center text-zinc-800 bg-zinc-200 hover:bg-orange-500 hover:text-white rounded-sm ">
+            <IoIosArrowBack />
+          </button>
+          <button className="customNext w-9 h-9  text-2xl flex justify-center items-center text-zinc-800 bg-zinc-200 hover:bg-orange-500 hover:text-white rounded-sm ">
+            <IoIosArrowForward />
+          </button>
         </div>
-        </div>
+
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation]}
+          className="mySwiper"
+          navigation={{
+            nextEl: ".customNext",
+            prevEl: ".customPrev",
+          }}
+          loop={true}
+          breakpoints={{
+            640: {slidesPerView: 1, spaceBetween:20},
+            768: {slidesPerView: 2, spaceBetween:20},
+            1024: {slidesPerView: 3, spaceBetween:20},
+          }}
+        >
+          {CustomerReviewData.map((item) => {
+            return (
+              <SwiperSlide key={item.id} className="bg-zinc-100 rounded-xl md:p-8 p-5">
+                <div className="flex items-center gap-5">
+                  <div>
+                    <img src={item.image} className="md:w-16 w-12 md:h-16 h-12 bg-red-300 rounded-full outline-2 outline-orange-500 outline-offset-3" />
+                  </div>
+                  <div className="">
+                    <h5 className="text-xl font-semibold">{item.name}</h5>
+                    <p className="text-zinc-600 mb-2">{item.profession}</p>
+                    <span className="flex gap-1 text-sm text-orange-400">
+                        {Array.from({length: item.rating}, (_, index)=>(
+                            <FaStar />
+                        ) )}
+                    </span>
+                  </div>
+                </div>
+                <div className="md:mt-10 mt-5 min-h-[15vh]">
+                  <p className="text-zinc-600">
+                    {item.para}
+                  </p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testomonials
+export default Testomonials;
